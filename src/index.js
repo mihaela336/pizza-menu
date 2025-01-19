a19cab07aaceee73014762551f85ad132e664a26
mihaela336 <mihaela.pasca336@gmail.com>
2025-01-19 14:48:35 +0200

    basic project structure
    -add type:module to project
    - create index.js
    -import react
    -create & render simple App component

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//create component
+function App() {
+  return <h1> Hell-o</h1>;
+}
+
+//render to dom
+//create object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//render component
+root.render(<App />);
