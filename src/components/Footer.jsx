73d8b8fb1c00a782403244d122e9244a2f539847
mihaela336 <mihaela.pasca336@gmail.com>
2025-01-19 18:19:11 +0200

    add closing time logic to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (hour >= openHour && hour <= closeHour) {
+  //     alert("We're currently open!");
+  //   } else {
+  //     alert("Sorry we are closed :(");
+  //   }
+
   return (
     <footer>
       <p>
-        {new Date().toLocaleTimeString()} We're open until 22:00. Order or visit
-        our shop online!
+        {isOpen
+          ? `We're open until ${closeHour}:00! Order
+        or visit our shop online!`
+          : `We are closed, opening at ${openHour}:00!`}
       </p>
       <button>Order</button>
     </footer>
