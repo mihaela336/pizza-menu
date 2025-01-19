de14ec50c7f6f47b4a19a0fdcfdf934ef4df8970
mihaela336 <mihaela.pasca336@gmail.com>
2025-01-19 17:10:24 +0200

    create hardcoded pizza component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pizzaData from "./data.js";
 
 //create component
 function App() {
-  return <h1> Hell-o</h1>;
+  return (
+    <div>
+      <h1> Hell-o</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="Focaccia"></img>
+      <h2>Focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
 }
 
 //render to dom
