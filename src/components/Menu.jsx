8bb43d3280b9bfdb8ba2314e540a453db0a70860
mihaela336 <mihaela.pasca336@gmail.com>
2025-01-21 16:51:13 +0200

    add props to pizzza component pt 1

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,9 +7,15 @@ function Menu() {
       <h2> Our Menu</h2>
       <p>Authentic italian pizza</p>
       <div className="pizzas">
-        <Pizza />
-        <Pizza />
-        <Pizza />
+        <Pizza
+          className="pizza"
+          //order in which we pass the props is irelevant
+          name="Focaccia"
+          photoName="pizzas/focaccia.jpg"
+          ingredients="Bread with italian olive oil and rosemary"
+          price={6}
+          soldOut="false"
+        />
       </div>
     </main>
   );
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -3,12 +3,15 @@ import pizzaData from "../data.js";
 
 //create component
 
-function Pizza() {
+function Pizza(props) {
   return (
     <div className="pizza">
-      <img src="pizzas/focaccia.jpg" alt="Focaccia"></img>
-      <h3>Focaccia</h3>
-      <p>Bread with italian olive oil and rosemary</p>
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
