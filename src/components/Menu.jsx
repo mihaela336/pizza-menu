7ee51cfde4d0d2c31222c67d9a0faf2fe04689d8
mihaela336 <mihaela.pasca336@gmail.com>
2025-01-21 20:09:17 +0200

    map pizzaData to pizza - the meh way

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import Pizza from "./Pizza.jsx";
+import pizzaData from "../data.js";
 
 function Menu() {
   return (
     <main className="menu">
       <h2> Our Menu</h2>
+
       <p>Authentic italian pizza</p>
       <div className="pizzas">
-        <Pizza
-          className="pizza"
-          //order in which we pass the props is irelevant
-          name="Focaccia"
-          photoName="pizzas/focaccia.jpg"
-          ingredients="Bread with italian olive oil and rosemary"
-          price={6}
-          soldOut="false"
-        />
+        <div>
+          {pizzaData.map((pizza) => {
+            return (
+              <Pizza
+                name={pizza.name}
+                photoName={pizza.photoName}
+                ingredients={pizza.ingredients}
+                price={pizza.price}
+                soldOut={pizza.soldOut}
+              />
+            );
+          })}
+        </div>
       </div>
     </main>
   );
