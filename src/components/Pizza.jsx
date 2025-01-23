5bbc140810c1cf62083f9ca88213c19c42517809
mihaela336 <mihaela.pasca336@gmail.com>
2025-01-23 19:16:18 +0200

    add conditional class& text to sold out pizza

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -3,14 +3,16 @@ import React from "react";
 //create component
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    // <li className={pizzaObj.soldOut ? "pizza sold-out" : "pizza"}> working
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      {/* using template literal ->makes  code less repetitive */}
       <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
